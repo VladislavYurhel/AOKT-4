6576fb198e33eef4f7aa9ee2149d90b08a6464e8
VladislavYurhel <worldstes@gmail.com>
2015-12-20 21:59:50 +0300

    Added

---
+++ AOKT4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOKT4
+{
+    class Program
+    {
+        static SerialPort serialPort;
+
+        static void Main(string[] args)
+        {
+
+           // newThread thOne = new newThread();
+
+            int portSpeed, dataBits;
+            string parity, stopBeats, port;
+            string message = "", username = "";
+
+            Console.Write("Порт: ");
+            port = Console.ReadLine();
+            Console.Write("Скорость передачи: ");
+            portSpeed = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Паритет: ");
+            parity = Console.ReadLine();
+            Console.Write("Биты данных: ");
+            dataBits = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Стоп биты: ");
+            stopBeats = Console.ReadLine();
+            Console.Write("Ваше имя: ");
+            username = Console.ReadLine();
+            initPort(port, portSpeed, parity, stopBeats, dataBits);
+            while (message != "/exit")
+            {
+                Console.Write("Вы: ");
+                message = Console.ReadLine();
+                serialPort.WriteLine(username + ": " + message);
+            };
+        }
+
+        static string getData()
+        {
+            string data = serialPort.ReadLine();
+            return data.Trim();
+        }
+
+        static void initPort(string sPort, int sSpeed, string sParity, string sStopBeats, int sDataBits)
+        {
+            serialPort = new SerialPort(sPort, sSpeed, (Parity)Enum.Parse(typeof(Parity), sParity), sDataBits,
+                (StopBits)Enum.Parse(typeof(StopBits), sStopBeats));
+            serialPort.Open();
+            serialPort.ErrorReceived += new SerialErrorReceivedEventHandler(serialPort_ErrorReceived);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+            Console.WriteLine("\nПорт открыт!\n");
+        }
+
+        static void serialPort_SendData(byte[] data)
+        {
+            serialPort.RtsEnable = true;
+            serialPort.Write(data, 0, data.Length);
+            Thread.Sleep(100);
+            serialPort.RtsEnable = false;
+        }
+
+        static void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            Thread.Sleep(100);
+            Console.Write("\r" + data);
+            Console.Write("\nВы: ");
+        }
+
+        static private void serialPort_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AOKT4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AOKT4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AOKT4")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4225ef07-b833-4cad-8c57-03f9ee55967b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
